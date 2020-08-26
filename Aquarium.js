4a6ddae6e04c268af1b88c019a49fa25cb13f9fd
Sanuka-bit <69225636+Sanuka-bit@users.noreply.github.com>
2020-08-26 12:40:10 +1000

    Update Aquarium.js
    Finished the code but the html file isn't working.

--- Aquarium.js
+++ Aquarium.js
@@ -4,15 +4,14 @@ window.onload=function() {
   widthObj = document.getElementById('txtWidth');
   costObj = document.getElementById('tdCost');
   document.getElementById('btnReset').onclick = resetInputs;
-  document.getElementById('btnCalc').onclick = calcWeight;
-}
+  document.getElementById('btnCalc').onclick = calcCost;
 function resetInputs() {
   lengthObj.value = '';
   heightObj.value = '';
   widthObj.value = '';
   costObj.innerHTML = '';
 }
-function calcWeight() {
+function calcCost() {
   var length = new Number(lengthObj.value);
   var height = new Number(heightObj.value);
   var width = new Number(widthObj.value);
@@ -21,8 +20,43 @@ function calcWeight() {
     alert('Invalid inputs');
     return;
     }
+}
 	
 function surfaceArea () {
-	
-costObj.innerHTML = (height*length + );
+	var length = new Number(lengthObj.value);
+	var height = new Number(heightObj.value);
+    var width = new Number(widthObj.value);
+return (length*height*2) + (length*width*2) + (width*height);
 }
+
+
+function glassCosts () {
+	var length = new Number(lengthObj.value);
+	var height = new Number(heightObj.value);
+    var width = new Number(widthObj.value);
+return	if (height > 25) {
+		(surfaceArea*0.1)
+	    }else {
+		(surfaceArea*0.06)
+	    }
+}
+function glueCosts () {
+ 	var length = new Number(lengthObj.value);
+	var height = new Number(heightObj.value);
+    var width = new Number(widthObj.value);	
+return ((height*4) + (length*2) + (width*2)*0.1)
+}
+
+function labourCosts () {
+	var length = new Number(lengthObj.value);
+	var height = new Number(heightObj.value);
+    var width = new Number(widthObj.value);	
+return (surfaceArea/100)
+}
+
+var grandTotal = (glassCosts () + glueCosts ()+ labourCosts()) *1.1)
+
+costObj.innerHTML = grandTotal;	
+	
+	
+
