a2f6c6870582202d1e7e22a6204f786897ff7897
Sanuka-bit <69225636+Sanuka-bit@users.noreply.github.com>
2020-09-10 07:28:37 +1000

    Finshed functions, all seem to be working.
    Only issue is the final CalcCost which is not working

--- Aquarium.js
+++ Aquarium.js
@@ -4,59 +4,69 @@ window.onload=function() {
   widthObj = document.getElementById('txtWidth');
   costObj = document.getElementById('tdCost');
   document.getElementById('btnReset').onclick = resetInputs;
-  document.getElementById('btnCalc').onclick = calcCost;
+  document.getElementById('btnCalcCost').onclick = CalcCost;
+}
+
+labourCosts = 2
+glassCost = 1.2
+glueCosts = 1.6
+gst = 0.48
+
+
 function resetInputs() {
   lengthObj.value = '';
   heightObj.value = '';
   widthObj.value = '';
   costObj.innerHTML = '';
 }
-function calcCost() {
-  var length = new Number(lengthObj.value);
-  var height = new Number(heightObj.value);
-  var width = new Number(widthObj.value);
-  costObj.innerHTML = '';
-  if(isNaN(length) || isNaN(height) || isNaN(width) ){
-    alert('Invalid inputs');
-    return;
-    }
-}
-	
+
 function surfaceArea () {
 	var length = new Number(lengthObj.value);
 	var height = new Number(heightObj.value);
     var width = new Number(widthObj.value);
-return (length*height*2) + (length*width*2) + (width*height);
+costObj.innerHTML = (length*height*2) + (length*width*2) + (width*height);
 }
 
-
-function glassCosts () {
-	var length = new Number(lengthObj.value);
-	var height = new Number(heightObj.value);
-    var width = new Number(widthObj.value);
-return	if (height > 25) {
-		(surfaceArea*0.1)
-	    }else {
-		(surfaceArea*0.06)
-	    }
-}
 function glueCosts () {
  	var length = new Number(lengthObj.value);
 	var height = new Number(heightObj.value);
     var width = new Number(widthObj.value);	
-return ((height*4) + (length*2) + (width*2)*0.1)
+return  ((height*4) + (length*2) + (width*2)*0.1);
 }
 
 function labourCosts () {
 	var length = new Number(lengthObj.value);
 	var height = new Number(heightObj.value);
     var width = new Number(widthObj.value);	
-return (surfaceArea/100)
+return (surfaceArea()/100);
 }
 
-var grandTotal = (glassCosts () + glueCosts ()+ labourCosts()) *1.1)
+function glassCost() {
+	var length = new Number(lengthObj.value);
+	var height = new Number(heightObj.value);
+    var width = new Number(widthObj.value);	
+	var costs;
+if (height > 25) {
+		costs = surfaceArea()*0.1;
+	    }else {
+		costs = surfaceArea()*0.06;
+	    }
+return costs;	
+}
 
-costObj.innerHTML = grandTotal;	
-	
-	
+function CalcCost() {
+	var length = new Number(lengthObj.value);
+	var height = new Number(heightObj.value);
+    var width = new Number(widthObj.value);	
+	var gc = glassCost();
+	var lc = labourCosts();
+	var glc = glueCosts();
 
+if(isNaN(length) || isNaN(width) || isNaN(height)) {
+	alert('Invalid length, width or height');
+	return;
+}
+
+costObj.innerHTML = '';	
+costObj.innerHTML = (gc + lc + glc *1.1);
+}
